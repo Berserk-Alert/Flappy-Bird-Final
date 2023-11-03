2410f2c1ad9e250c54adc4530fc3ed7c44bd4860
unknown <RHS@RHS-M2A-36.rusd.local>
2023-11-03 14:13:44 -0700

    Pipe spawn

--- Flappy Bird/Assets/PipeMove.cs
+++ Flappy Bird/Assets/PipeMove.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pipe : MonoBehaviour
+public class PipeMove : MonoBehaviour
 {
-
-    public float moveSpeed = 5;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +14,5 @@ public class Pipe : MonoBehaviour
     void Update()
     {
         
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
-
     }
 }
